dbf5e978312e2f0e44c6366745a2198011b855c8
Tahcin Ul Karim Mycin <mycin.mit@gmail.com>
2025-04-13 01:46:08 +0600

    cli text edit2

--- pub.sh
+++ pub.sh
@@ -1,4 +1,8 @@
-npm unlink -g zigimport
+echo "Building rustport..."
+npm unlink -g rustport
 bun run build
+echo "Linking rustport..."
 npm link
+echo "Publishing to npm..."
 npm publish
+echo "Published..."
